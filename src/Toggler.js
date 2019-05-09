94ffca4fb59154f6d9ff4ff42b77c9da4176f9e2
Thiago Giammattey <giammattey@gmail.com>
2019-05-09 14:28:04 -0300

    add toggleIsHappy and toggleIsHeartbroken

--- src/Toggler.js
+++ src/Toggler.js
@@ -2,12 +2,17 @@ import React, { useState } from 'react';
 
 function Toggler() {
 	const [isHappy, setIsHappy] = useState(true);
-	const toogle = () => {
+	const [isHeartbroken, setIsHeartbroken] = useState(false);
+	const toogleIsHappy = () => {
 		setIsHappy(!isHappy);
+	};
+	const toogleIsHeartbroken = () => {
+		setIsHeartbroken(!isHeartbroken);
 	};
 	return (
 		<div>
-			<h1 onClick={toogle}>{isHappy ? '😄' : '😢'}</h1>
+			<h1 onClick={toogleIsHappy}>{isHappy ? '😄' : '😢'}</h1>
+			<h1 onClick={toogleIsHeartbroken}>{isHeartbroken ? '💔' : '❤️'}</h1>
 		</div>
 	);
 }
