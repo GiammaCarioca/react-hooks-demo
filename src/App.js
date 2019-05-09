6dc1ce84388d8cecbc97dd6ca96fb6bedc943872
Thiago Giammattey <giammattey@gmail.com>
2019-05-09 15:36:40 -0300

    add component with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import CounterClass from './CounterClass';
-import CounterHooks from './CounterHooks';
-import Toogler from './Toggler';
-import SimpleFormClass from './SimpleFormClass';
-import SimpleFormHooks from './SimpleFormHooks';
-import SimpleFormInputHook from './SimpleFormInputHook';
+// import CounterClass from './CounterClass';
+// import CounterHooks from './CounterHooks';
+// import Toogler from './Toggler';
+// import SimpleFormClass from './SimpleFormClass';
+// import SimpleFormHooks from './SimpleFormHooks';
+// import SimpleFormInputHook from './SimpleFormInputHook';
+import Clicker from './Clicker';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
-			<CounterClass />
+			{/* <CounterClass />
 			<CounterHooks />
 			<Toogler />
 			<SimpleFormClass />
 			<SimpleFormHooks />
-			<SimpleFormInputHook />
+			<SimpleFormInputHook /> */}
+			<Clicker />
 		</div>
 	);
 }
